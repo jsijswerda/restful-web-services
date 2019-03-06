7cc53a4619e459979fd73e0ab7d4f24d51abd5df
Jasper Sijswerda <jsijswerda@sogyo.nl>
2019-03-06 12:18:11 +0100

    Initial commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,33 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean{
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+
+
+
+	public HelloWorldBean(String message) {
+		this.setMessage(message);
+	}
+
+	
+	
+	
+	@Override
+	public String toString() {
+		return String.format("HelloWorldBean [message=%s]", message);
+	}
+
+ 
+	
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
